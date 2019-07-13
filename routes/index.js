cc15f7a2ccf6b7a6ebd742fde1ccf7a2b3db1b2a
Pakeenai Khamlaeng <pakeenai169@gmail.com>
2019-07-13 16:15:08 +0700

    เชื่อมดาต้าเบส

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var connect = require("../config/database")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'หน้าแรก' });
+router.get('/', function (req, res, next) {
+  var query = "SELECT * FROM positions";
+  connect.query(query, function (error, result) {
+    console.log(error)
+    res.json(result)
+  })
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('about', { title: 'เกี่ยวกับ' });
 });
 
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
   res.render('contact', { title: 'ติดต่อเรา' });
 });
 
